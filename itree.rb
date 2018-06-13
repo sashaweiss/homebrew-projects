84200f8ea3265f233c7fb9531d470a10e843325c
sashaweiss <sasha@sashaweiss.coffee>
2018-06-13 14:38:33 -0500

    Bump itree version and let brew infer it automatically

--- itree.rb
+++ itree.rb
@@ -1,10 +1,9 @@
 class Itree < Formula
   desc "Interactively navigable version of `tree`, built in Rust"
   homepage "https://github.com/sashaweiss/itree"
-  url "https://github.com/sashaweiss/itree/archive/0.2.0.tar.gz"
+  url "https://github.com/sashaweiss/itree/archive/0.2.2.tar.gz"
   sha256 "a48aa3367a42a256a185cdaa53ed318e31c4c3219f5d36dc5f6cbe9e1152591c"
   head "https://github.com/sashaweiss/itree.git"
-  version "0.2.0"
 
   depends_on "rust" => :build
 
