adcea332d455e9824a6867102d1ef0995248e6fc
sashaweiss <sasha@sashaweiss.coffee>
2018-06-13 14:39:48 -0500

    Update SHA256 for most recent itree

--- itree.rb
+++ itree.rb
@@ -2,7 +2,7 @@ class Itree < Formula
   desc "Interactively navigable version of `tree`, built in Rust"
   homepage "https://github.com/sashaweiss/itree"
   url "https://github.com/sashaweiss/itree/archive/0.2.2.tar.gz"
-  sha256 "a48aa3367a42a256a185cdaa53ed318e31c4c3219f5d36dc5f6cbe9e1152591c"
+  sha256 "b3389ab8b0b047de7404bbb120f044f426d287a1b010fa601999894bcd2b09c1"
   head "https://github.com/sashaweiss/itree.git"
 
   depends_on "rust" => :build
